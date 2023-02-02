20c8e5bb3866ccb4fda1e43dc3f8ddc667c377e8
vivaloz <olafjanse10@gmail.com>
2023-02-02 23:35:52 +0100

    added translation menu and fixed css cursor property

--- script/main.js
+++ script/main.js
@@ -13,3 +13,62 @@ document.addEventListener('contextmenu', event => {
     if(!mousedOver) return
     event.preventDefault();
 });
+
+// translate
+let translations = {
+    "submitEuros":{
+        "en": "submit",
+        "nl": "indienen"
+    },
+    "submitMagnet":{
+        "en": "submit",
+        "nl": "indienen"
+    },
+    "submitGravity":{
+        "en": "submit",
+        "nl": "indienen"
+    },
+    "buttonAdvanced":{
+        "en": "Advanced features",
+        "nl": "Geavanceerde functies"
+    },
+    "magStrengthLabel":{
+        "en": "Strength",
+        "nl": "Kracht"
+    },
+    "gravityValLabel":{
+        "en": "Strength",
+        "nl": "Kracht"
+    },
+    "doctitle":{
+        "en": "Pinball Physics",
+        "nl": "Pinball-fysica"
+    }
+}
+function doTranslation(lan){
+    setFlagBrightness(lan)
+    if(lan == "en"){
+        rayOutput.html("Amount: "+rayCount.elt.value);
+    } else if(lan == "nl"){
+        rayOutput.html("Aantal: "+rayCount.elt.value);
+    }
+    for(let temp in translations){
+        try{
+            document.querySelector("#"+temp).innerHTML = translations[temp][lan];
+        } catch(error){
+            console.error(error);
+        }
+    }
+}
+var possibleLanguages = ['en', 'nl'];
+function setFlagBrightness(lan){
+    let lanid = 'lang'+lan;
+    document.getElementById(lanid).style.filter = 'opacity(100%)'
+    console.log('%cbrightness.flag.'+lan+' has been set to '+document.getElementById(lanid).style.filter,"color: lightgreen;")
+    for(let temp of possibleLanguages){
+        if(temp == lan) continue;
+        let id = 'lang'+temp;
+        document.getElementById(id).style.filter = 'opacity(60%)'
+        console.log('%cbrightness.flag.'+temp+' has been set to '+document.getElementById(id).style.filter,"color: green;")
+    }
+}
